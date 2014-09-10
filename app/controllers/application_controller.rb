a3f4e0579e3149f0232e9ca483f95935405a45d1
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-10 16:45:51 -0400

    set up stripe models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def new
+
+  end
+
+  def create
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => 'example@stripe.com',
+      :cart => params[:stripeToken]
+      )
+
+    charge = Stripe::Charge.create(
+      :customer => customer.id,
+      :amount => @amount,
+      :description => 'Rails Stripe customer',
+      :currency => 'usd'
+      )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :clinics
+  resources :charges
   get '/about' => 'home#about'
   get '/faq' => 'home#faq'
   get '/contact' => 'home#contact'
