bad67ef5b67930fd1bdf8161752b74fc37655a52
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-10 12:30:27 -0400

    created clinic index page and new page. on index page delete button deletes clinic object, and sends notification into flash:alert

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,4 +1,38 @@
 class ClinicsController < ApplicationController
 
+  def index
+    @clinics = Clinic.all
+  end
+
+  def new
+    @clinic = Clinic.new
+  end
+
+  def create
+    clinic = Clinic.new(clinic_params)
+    clinic.remaining_spots = clinic.total_spots
+    clinic.save!
+    redirect_to clinics_path
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @clinic = Clinic.find(params[:id])
+    flash[:alert] = "#{@clinic.name} has been successfully"
+    @clinic.destroy!
+    redirect_to clinics_path
+  end
+
+  private
+  def clinic_params
+    params.require(:clinic).permit(:name, :price, :date_time, :location_name, :location_street_one, :location_street_two, :city, :state, :zipcode, :total_spots)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  resources :clinics
   get '/about' => 'home#about'
   get '/faq' => 'home#faq'
   get '/contact' => 'home#contact'
