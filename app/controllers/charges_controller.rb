921ea9ac004443c7f23cb9b0b7ea2818797a00a0
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-11 10:54:17 -0400

    stripe checkout

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,20 +5,21 @@ class ChargesController < ApplicationController
   end
 
   def create
-    render text: params.inspect
-  #   Stripe.api_key = 'sk_test_vIZ2LXyYOSTNCMW72YZyhibo'
-  #   token = params[:stripeToken]
+    binding.pry
+    # Stripe.api_key = 'sk_test_vIZ2LXyYOSTNCMW72YZyhibo'
+    # token = params[:stripeToken]
+    # clinic_id = sessions[:clinic_id]
 
-  #   begin
-  #     charge = Stripe::Charge.create(
-  #       :amount => 1000,
-  #       :currency => 'usd',
-  #       :card => token,
-  #       :description => 'testing 123'
-  #       )
-  #   rescue Stripe::CardError => e
-  #     # The card ahsb een declined
-  #   end
+    # begin
+    #   charge = Stripe::Charge.create(
+    #     :amount => 1000,
+    #     :currency => 'usd',
+    #     :card => token,
+    #     :description => 'testing 123'
+    #     )
+    # rescue Stripe::CardError => e
+    #   # The card ahsb een declined
+    # end
   end
 
 
