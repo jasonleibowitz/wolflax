4613ea3c96a7a3e9b0baaf1245fa8f7f69bbc271
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-16 16:28:06 -0400

    using environment variables

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,7 @@
 Rails.configuration.stripe = {
   # :publishable_key => ENV['WOLF_PUBLISHABLE_KEY'],
-  :publishable_key => 'pk_test_zPWECKnej44Flev2zwZDcznJ',
-  # :secret_key      => ENV['WOLF_SECRET_KEY']
-  :secret_key      => 'sk_test_vIZ2LXyYOSTNCMW72YZyhibo'
+  :publishable_key => ENV["STRIPE_PUBLIC_KEY"],
+  :secret_key      => ENV["STRIPE_SECRET_KEY"]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
