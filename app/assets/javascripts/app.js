123d6dd4ebe99355dc2ae2047d4a4af4169b44aa
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-17 09:26:35 -0400

    sticky footer via jquery

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,33 @@
-$(document).ready(function(){
-  console.log('loaded bro!');
+$(window).bind("load", function() {
+  console.log('loaded bro');
+
+   var footerHeight = 0;
+       footerTop = 0;
+       $footer = $(".footer");
+
+   positionFooter();
+
+
+   $(window)
+           .scroll(positionFooter)
+           .resize(positionFooter);
 
 });
 
+function positionFooter() {
 
+  footerHeight = $footer.height();
+  footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+ if ( ($('.content-wrap').height()+footerHeight) < $(window).height()) {
+     $footer.css({
+          position: "absolute",
+          bottom: 0
+     });
+ } else {
+     $footer.css({
+          position: "static"
+     });
+ }
+
+}
