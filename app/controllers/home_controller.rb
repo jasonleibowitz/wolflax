f9333c49dfab6ba20826f49bf68c15e0fe224e4e
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-16 21:04:36 -0400

    fixed upcoming clinics on homepage to not show sold out clinics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def welcome
-    @clinics = Clinic.where("date_time > ?", DateTime.now).order(date_time: :asc).limit(3)
+    @clinics = Clinic.where("date_time > ?", DateTime.now).where("remaining_spots > ?", 0).order(date_time: :asc).limit(3)
   end
 
   def about
