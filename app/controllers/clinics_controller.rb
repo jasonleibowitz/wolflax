d0161b5059bcd7e2442cf3e0ba49230a32e3aacd
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-22 17:22:02 -0400

    created private method to capitalize each word in clinic name, location name and city name on new clinic create

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -22,6 +22,9 @@ class ClinicsController < ApplicationController
     @clinic = Clinic.new(clinic_params)
     if @clinic.valid?
       @clinic.date_time = Chronic.parse(params[:clinic][:date_time])
+      @clinic.name = capitalize_each_word(params[:clinic][:name])
+      @clinic.location_name = capitalize_each_word(params[:clinic][:location_name])
+      @clinic.city = capitalize_each_word(params[:clinic][:city])
       @clinic.remaining_spots = @clinic.total_spots
       @clinic.save!
       flash[:alert] = "#{@clinic.name} has been created successfully."
@@ -49,7 +52,7 @@ class ClinicsController < ApplicationController
 
   def destroy
     @clinic = Clinic.find(params[:id])
-    flash[:alert] = "#{@clinic.name} has been successfully"
+    flash[:alert] = "#{@clinic.name} has been successfully deleted."
     @clinic.destroy!
     redirect_to clinics_path
   end
@@ -57,6 +60,14 @@ class ClinicsController < ApplicationController
   private
   def clinic_params
     params.require(:clinic).permit(:name, :price, :description, :date_time, :location_name, :location_street_one, :location_street_two, :city, :state, :zipcode, :total_spots)
+  end
+
+  def capitalize_each_word(form_string)
+    result_array = []
+    form_string.split(' ').each do |word|
+      result_array.push(word.capitalize)
+    end
+    return result_array.join(' ')
   end
 
   # Method to increase remaining slots when total slots is updated
