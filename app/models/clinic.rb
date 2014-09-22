d80005350872df1841bd4ce8315e926e3447f0e7
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-22 18:02:59 -0400

    created scopes for sold_out and expired and created methods for sold_out? and expired? for instances of clinic class

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -5,66 +5,28 @@ class Clinic < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :students
 
+  scope :sold_out, -> { where(remaining_spots: 0) }
+  scope :expired, -> { where('date_time < ?', Date.today) }
+
   def update_attendance
     self.remaining_spots = self.total_spots - self.students.length
     self.save!
   end
 
-  def us_states
-    [
-      ['Alabama', 'AL'],
-      ['Alaska', 'AK'],
-      ['Arizona', 'AZ'],
-      ['Arkansas', 'AR'],
-      ['California', 'CA'],
-      ['Colorado', 'CO'],
-      ['Connecticut', 'CT'],
-      ['Delaware', 'DE'],
-      ['District of Columbia', 'DC'],
-      ['Florida', 'FL'],
-      ['Georgia', 'GA'],
-      ['Hawaii', 'HI'],
-      ['Idaho', 'ID'],
-      ['Illinois', 'IL'],
-      ['Indiana', 'IN'],
-      ['Iowa', 'IA'],
-      ['Kansas', 'KS'],
-      ['Kentucky', 'KY'],
-      ['Louisiana', 'LA'],
-      ['Maine', 'ME'],
-      ['Maryland', 'MD'],
-      ['Massachusetts', 'MA'],
-      ['Michigan', 'MI'],
-      ['Minnesota', 'MN'],
-      ['Mississippi', 'MS'],
-      ['Missouri', 'MO'],
-      ['Montana', 'MT'],
-      ['Nebraska', 'NE'],
-      ['Nevada', 'NV'],
-      ['New Hampshire', 'NH'],
-      ['New Jersey', 'NJ'],
-      ['New Mexico', 'NM'],
-      ['New York', 'NY'],
-      ['North Carolina', 'NC'],
-      ['North Dakota', 'ND'],
-      ['Ohio', 'OH'],
-      ['Oklahoma', 'OK'],
-      ['Oregon', 'OR'],
-      ['Pennsylvania', 'PA'],
-      ['Puerto Rico', 'PR'],
-      ['Rhode Island', 'RI'],
-      ['South Carolina', 'SC'],
-      ['South Dakota', 'SD'],
-      ['Tennessee', 'TN'],
-      ['Texas', 'TX'],
-      ['Utah', 'UT'],
-      ['Vermont', 'VT'],
-      ['Virginia', 'VA'],
-      ['Washington', 'WA'],
-      ['West Virginia', 'WV'],
-      ['Wisconsin', 'WI'],
-      ['Wyoming', 'WY']
-    ]
+  def sold_out?
+    if self.remaining_spots == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  def expired?
+    if self.date_time < Date.today
+      return true
+    else
+      return false
+    end
   end
 
 end
