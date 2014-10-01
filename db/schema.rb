569474affdad96ed92b42fee79666c59aef272eb
jasonleibowitz <jasonaleibowitz@gmail.com>
2014-09-30 21:26:43 -0400

    added ability for user to add position when signing up for clinic, and student's position shows up on admin student view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915161601) do
+ActiveRecord::Schema.define(version: 20141001011417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140915161601) do
     t.integer "clinic_id"
     t.string  "stripe_charge_token"
     t.integer "age"
+    t.string  "position"
   end
 
   create_table "users", force: true do |t|
