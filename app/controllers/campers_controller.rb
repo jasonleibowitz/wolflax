1751f926cf50cf73c90e3ad7bd09c626e1ddaae1
Jason Leibowitz <jason.leibowitz@tigerspike.com>
2015-03-07 12:33:45 -0500

    Started updating Campers MVC

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -12,15 +12,11 @@ class CampersController < ApplicationController
   def new
     @camp = Camp.find(params[:camp_id])
     @camper = @camp.campers.build
-    if @camper.camp.remaining_spots == 0
-      flash[:alert] = "This camp is currently sold out."
-    end
   end
 
   def create
     @camper = Camper.new(camper_params)
     if @camper.save_with_payment
-      @camper.camp.update_attendance
       redirect_to @camper.camp, :notice => "Thank you for your purchase. We look forward to seeing you!"
     else
       render :new
