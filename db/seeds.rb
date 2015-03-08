c884f597772ce80158451c0f15e964115bf9e973
Jason Leibowitz <jason.leibowitz@tigerspike.com>
2015-03-08 18:27:10 -0400

    updated ruby version and added admins in seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(first_name: "Jason", last_name: "Leibowitz", email: "jason@leibowitz.me", password: "sertgh67", password_confirmation: "sertgh67", admin: true)
+User.create(first_name: "Jordan", last_name: "Wolf", email: "jordan.wolf@duke.edu", password: "silverturtle", password_confirmation: "silverturtle", admin: true)
+User.create(first_name: "George", last_name: "Stein", email: "george_stein24@yahoo.com", password: "silverturtle", password_confirmation: "silverturtle", admin: true)
