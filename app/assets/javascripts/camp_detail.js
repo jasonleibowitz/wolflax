27e10747963af814798ee4bcc746e2c63a3b2a25
Jason Leibowitz <jason.leibowitz@tigerspike.com>
2015-03-09 22:20:31 -0400

    Added coundown clock to camp detail page

---
+++ app/assets/javascripts/camp_detail.js
@@ -0,0 +1,6 @@
+$(".camps.show").ready(function(){
+  var starting_date = $("#clock").data("start");
+  $("#clock").countdown(starting_date, function(event){
+    $(this).html(event.strftime('%D days %H hours %M minutes %S seconds'));
+  });
+});
