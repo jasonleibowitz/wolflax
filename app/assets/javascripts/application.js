907971f34545cf3f9b0d800907e6af1351401d90
Jason Leibowitz <jason.leibowitz@tigerspike.com>
2015-05-30 17:26:04 -0400

    Updated DOB input on new registration page to text field with js datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-readyselector
+//= require bootstrap-datepicker
 //= require_tree .
---
+++ app/assets/javascripts/newRegistration.js
@@ -0,0 +1,9 @@
+$(".campers.new").ready(function(){
+
+  $('#datepicker').datepicker({
+    format: 'mm-dd-yyyy',
+    autoclose: true,
+    startView: 2,
+  });
+
+});
